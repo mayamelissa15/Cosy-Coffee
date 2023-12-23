ed2de294670e0471ad7960f8257fc4388f4fa2be
Ms <125511982+mayamelissa15@users.noreply.github.com>
2023-12-23 16:11:19 +0000

    login/registration

--- FRONT/src/Login.js
+++ FRONT/src/Login.js
@@ -1,4 +1,4 @@
-import cupOfCoffe from "./Assets/6378d1abab345038ecf839cb_coffee_cookies-p-800.png"
+
 import { Link, useNavigate, useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 import { useState } from "react";
@@ -24,19 +24,13 @@ const Login = () => {
       });
 
   }
-    return (<div className="">
-      
-    <div className="  bg-[#F5F5DC] m-32 p-16 shadow-md ">
-        <div className="py-8">
-        <p className="text-md text-slate-400 font-medium  py-1">join our family and discover us </p>
-        <h1 className="text-3xl text-black font-semibold py-2">Create a new account</h1> 
-        <p className="text-sm text-black font-medium  py-2">Already a member ? <Link to='/Registration' className="text-[#7B3F00]"> Join us</Link></p>
-        </div>
-        <div className="flex justify-around ">
-        <img src={cupOfCoffe} className="h-[500px] w-[500px]"></img>
-        <div >
-            <form>
-        <div className=" mb-4 w-96 py-8">
+    return (
+      <div className="mx-auto my-32 w-1/2">
+    <div className=" bg-[#F5F5DC] rounded-xl  shadow-md ">
+        
+        <h1 className="text-3xl font-semibold  text-center p-8 text-[#7B3F00]">Connect to your account</h1> 
+        <form className="">
+        <div className=" mx-auto mb-4 w-96 py-8">
       <label htmlFor="name"
        className="block text-sm font-medium text-slate-700">UserName</label>
       <input type="text" id="name" name="name" 
@@ -46,7 +40,7 @@ const Login = () => {
       className="bg-slate-50 focus:outline-none mt-2 p-2 block w-full rounded-md  shadow-md after:content-['*'] after:ml-0.5 after:text-red-500" placeholder="John Doe" required></input>
     </div>
     
-    <div className=" mb-4 w-96 py-2">
+    <div className=" mx-auto mb-4 w-96 py-2">
       <label htmlFor="password" className="block text-sm font-medium text-slate-700">Password</label>
       <input type="password" id="password" name="password" 
        onChange={(e)=>{
@@ -55,13 +49,14 @@ const Login = () => {
       className="bg-slate-50  focus:outline-none mt-2 p-2 block w-full rounded-md   after:content-['*'] after:ml-0.5 after:text-red-500 shadow-md focus:border-none"  required></input>
     </div>
     
-    <div className="p-8">
-      <button type="submit" className="bg-[#7B3F00] text-white px-6 py-2 mx-2 rounded-full hover:shadow-md  hover:bg-[#F5F5DC] hover:text-[#7B3F00] duration-500 " onClick={Login}>Login</button>
+    <div className="flex justify-center  p-8">
+      <button type="submit" className="mx-auto bg-[#7B3F00] text-white px-6 py-2 mx-2 rounded-full hover:shadow-md  hover:bg-[#F5F5DC] hover:text-[#7B3F00] duration-500 " onClick={Login}>Login</button>
     </div>
     <h1 className="text-color-500 text-md font-medium">{loginStatus}</h1>
-        </form></div>
-        </div> </div>
-    </div>  );
+        </form>
+      </div></div>
+      
+     );
 }
  
 export default Login;
--- FRONT/src/Registration.js
+++ FRONT/src/Registration.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import Axios from "axios";
+import cupOfCoffe from "./Assets/6378d1abab345038ecf839cb_coffee_cookies-p-800.png"
+import { Link } from "react-router-dom";
 
 
 const Registration = () => {
@@ -17,12 +19,17 @@ const Registration = () => {
 
     }
 
-    return ( <div className="bg-[#F5F5DC] mx-96 my-32 shadow-md ">
+    return ( <div className="bg-[#F5F5DC] mx-auto my-32 w-[960px] p-10 shadow-md ">
         
-    <div className="py-2 text-center">
-    <h1 className="text-3xl font-semibold  m-8 text-[#7B3F00]">Connect to your account</h1> 
- 
+    <div className="py-2 ">
+    <p className="text-md text-slate-400 font-medium  py-1">join our family and discover us </p>
+        <h1 className="text-3xl text-black font-semibold py-2">Create a new account</h1> 
+        <p className="text-sm text-black font-medium  py-2">Already a member ? <Link to='/Login' className="text-[#7B3F00]"> Join us</Link></p>
+
     </div>
+    <div className="flex justify-around ">
+        <img src={cupOfCoffe} className="h-[500px] w-[500px]"></img>
+        <div ></div>
         <form >
     <div className=" mx-auto mb-4 w-96 py-8">
   <label for="name" className="block text-sm font-medium text-slate-700">UserName</label>
@@ -46,7 +53,7 @@ const Registration = () => {
   <button  className=" bg-[#7B3F00] text-white px-6 py-2 mx-2 rounded-full hover:shadow-md  hover:bg-[#F5F5DC] hover:text-[#7B3F00] duration-500 " onClick={Registration}>Register</button>
 </div>
     </form></div>
- 
+ </div>
  );
 }
  
